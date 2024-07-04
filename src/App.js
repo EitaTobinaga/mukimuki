1a2b7289df8ea7a5247d686019380e6368f15fdf
Eita <okomedayo@icloud.com>
2024-07-04 23:10:25 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  <link rel=''
+  return (
+    <body>
+      hello world
+      <div className='gazou'>
+        <img src='https://user0514.cdnw.net/shared/img/thumb/044sukagamino17103_TP_V.jpg' alt=''></img>
+        <p className='gazoumoji'>筋肉計算</p>
+      </div>
+        <h1>Let's go to <a href='https://www.google.com/'>google</a></h1>
+        <nav>
+          <ul>
+            <p>this is <a href='https://www.e-healthnet.mhlw.go.jp/information/dictionary/metabolic/ym-002.html#:~:text=BMI%EF%BC%88BMI%EF%BC%89&text=%EF%BC%BB%E4%BD%93%E9%87%8D%EF%BC%88kg%EF%BC%89%EF%BC%BD%C3%B7,m%E3%81%A7%E8%A8%88%E7%AE%97%E3%81%97%E3%81%BE%E3%81%99%EF%BC%89%E3%80%82'>BMI</a></p>
+          </ul>
+        </nav>
+      <main></main>
+      <footer></footer>
+    </body>
+  );
+}
+export default App;
